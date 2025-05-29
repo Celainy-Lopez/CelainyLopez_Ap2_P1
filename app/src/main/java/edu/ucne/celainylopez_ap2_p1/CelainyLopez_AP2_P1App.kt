5066451b88730518cc32966b8c53e24965bbfe2e
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-28 20:44:55 -0400

    Configura inyección dependencia con Hilt

---
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/CelainyLopez_AP2_P1App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.celainylopez_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CelainyLopez_Ap2_P1App: Application() {
+}
--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.celainylopez_ap2_p1.ui.theme.CelainyLopez_Ap2_P1Theme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
