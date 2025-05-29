309670238ac455d379afbe1ddb7e281926907123
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-28 23:11:44 -0400

    Agrega AppModule

---
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/di/AppModule.kt
@@ -0,0 +1,15 @@
+package edu.ucne.celainylopez_ap2_p1.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+}
