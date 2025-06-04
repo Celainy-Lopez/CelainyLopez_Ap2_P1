79958cee0b6ef063bbce69ec71b8111756a12508
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-04 19:25:47 -0400

    Eventos de la tarea creados

--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/presentation/tareas/TareaEvent.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/presentation/tareas/TareaEvent.kt
@@ -1,5 +1,11 @@
 package edu.ucne.celainylopez_ap2_p1.presentation.tareas
 
-sealed interface TareaEvent {
+interface TareaEvent {
+    data class TareaChange(val tareaId: Int): TareaEvent
+    data class DescripcionChange(val descripcion: String): TareaEvent
+    data class TiempoChange(val tiempo: Int): TareaEvent
 
+    data object Save: TareaEvent
+    data object Delete: TareaEvent
+    data object New: TareaEvent
 }
