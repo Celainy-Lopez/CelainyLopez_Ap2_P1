22c83a107452cc5b144363c4216eae2fef65e084
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-04 19:04:27 -0400

    Base de datos creada

--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/database/TareaDb.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/database/TareaDb.kt
@@ -1,4 +1,18 @@
 package edu.ucne.celainylopez_ap2_p1.data.local.database
 
-class TareaDb {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.celainylopez_ap2_p1.data.local.dao.TareaDao
+import edu.ucne.celainylopez_ap2_p1.data.local.entities.TareaEntity
+
+@Database(
+    entities = [
+        TareaEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class TareaDb: RoomDatabase(){
+    abstract fun TareaDao(): TareaDao
 }
