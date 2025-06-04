31a0f6a8aa2a14023e27386cba3950d12dfab04f
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-04 18:58:04 -0400

    Entidad creada

--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/entities/Tareas.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/entities/Tareas.kt
@@ -1,4 +1,12 @@
 package edu.ucne.celainylopez_ap2_p1.data.local.entities
 
-class Tareas {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tareas")
+data class TareaEntity (
+    @PrimaryKey
+    val tareaId: Int? = null,
+    val descripcion: String = "",
+    val tiempo: Int = 0
+)
