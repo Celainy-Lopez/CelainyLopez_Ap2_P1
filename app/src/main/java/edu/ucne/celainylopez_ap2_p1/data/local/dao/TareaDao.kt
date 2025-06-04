c151a388157d3a8ebee07b76ea2fec5516231ca6
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-04 19:03:37 -0400

    Dao creado

--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/dao/TareaDao.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/data/local/dao/TareaDao.kt
@@ -1,5 +1,32 @@
 package edu.ucne.celainylopez_ap2_p1.data.local.dao
 
-interface TareaDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.celainylopez_ap2_p1.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
 
+
+@Dao
+interface TareaDao {
+    @Upsert
+    suspend fun save(tarea: TareaEntity)
+
+    @Query(
+        """
+            SELECT *
+            FROM Tareas
+            WHERE tareaId =:id
+            LIMIT 1
+        """
+    )
+
+    suspend fun find(id: Int): TareaEntity?
+
+    @Delete
+    suspend fun delete(tarea: TareaEntity)
+
+    @Query("SELECT * FROM Tareas")
+    fun getAll(): Flow<List<TareaEntity>>
 }
