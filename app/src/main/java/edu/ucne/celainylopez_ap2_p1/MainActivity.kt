f825b707a3a0adcc737571ee4ad9a24f963542bf
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-04 21:37:58 -0400

    Main actualizado

--- app/src/main/java/edu/ucne/celainylopez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p1/MainActivity.kt
@@ -4,28 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.celainylopez_ap2_p1.presentation.navigation.HostNavigation
+import edu.ucne.celainylopez_ap2_p1.presentation.navigation.HomeNavHost
 import edu.ucne.celainylopez_ap2_p1.ui.theme.CelainyLopez_Ap2_P1Theme
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             CelainyLopez_Ap2_P1Theme {
                 val nav = rememberNavController()
-                HostNavigation(nav)
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        HomeNavHost(
+                            navHostController = nav
+                        )
+                    }
+                }
             }
         }
     }
